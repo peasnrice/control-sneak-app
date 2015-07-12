36d2156efbeb85c4661e383789111cf3f26fb1c4
Andrew Evans <andrew.j.c.e@gmail.com>
2015-07-12 18:18:45 -0400

    Added ngCordova library for oauth, modified index, app & controllers to support ngCordova

--- README.md
+++ README.md
@@ -12,9 +12,15 @@ from within the application directory run:
 
 `npm install`
 
+###install some fancy plugins and dependencies
+This plugin will allow you to login using facebook oauth using the facebook apps dev app.
+
+`cordova plugin add https://github.com/Wizcorp/phonegap-facebook-plugin.git --variable APP_ID="1678602979038851" --variable APP_NAME="ControlSneak - dev"`
+
 ##Prepare & Run Project
 `ionic platform add ios`
 
 `ionic build ios`
 
 `ionic emulate ios`
+
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,45 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $cordovaFacebook) {
+    
+  $scope.facebookLogin = function () {
+      console.log("not already logged in");
+      $cordovaFacebook.login(["public_profile", "email", "user_friends"]).then(function(success) {
+          $scope.access_token = success.authResponse.accessToken;
+          $http.post("http://localhost:8100/rest-auth/facebook/", {access_token: $scope.access_token}).then(function(success) {
+            window.localStorage['app_token'] = success.data.key;
+            console.log(window.localStorage['app_token']);
+            $http.defaults.headers.common['Authorization'] = "Token " + window.localStorage['app_token'];
+            console.log("Success");
+            $state.go('tab.play');
+            // console.log("we got code: " + $scope.app_token);
+            }, function(error) {
+              // console.log("couldn't get django api key");
+          });
+       }, function (error) {
+        console.log("facebook login failed");
+       });
+     // });
+   };
+
+  $scope.logout = function () {
+    $http.post("http://localhost:8100/rest-auth/logout/").then(function(success) {
+          $http.defaults.headers.common['Authorization'] = undefined;
+          $cordovaFacebook.logout()
+            .then(function(success) {
+              console.log("logout succesful");
+            }, function (error) {
+              console.error('ERR', err);
+              $state.go('signin');
+            });
+          $state.go('signin');
+        }, function(err) {
+          console.error('ERR', err);
+          $state.go('signin');
+    });
+  };
+    
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
