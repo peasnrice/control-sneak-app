e49e83ae94a595e9adc0b3daefe43cc541a773e4
Andrew Evans <andrew.j.c.e@gmail.com>
2015-07-12 23:10:37 -0400

    Added groundwork for facebook authentication

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,18 +1,20 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $cordovaFacebook) {
+.controller('DashCtrl', function($scope, $http, $state, $cordovaFacebook) {
     
   $scope.facebookLogin = function () {
       console.log("not already logged in");
       $cordovaFacebook.login(["public_profile", "email", "user_friends"]).then(function(success) {
           $scope.access_token = success.authResponse.accessToken;
-          $http.post("http://localhost:8100/rest-auth/facebook/", {access_token: $scope.access_token}).then(function(success) {
-            window.localStorage['app_token'] = success.data.key;
-            console.log(window.localStorage['app_token']);
-            $http.defaults.headers.common['Authorization'] = "Token " + window.localStorage['app_token'];
-            console.log("Success");
-            $state.go('tab.play');
-            // console.log("we got code: " + $scope.app_token);
+          console.log($scope.access_token);
+          $http.post("http://localhost:3000/login/facebook/", {access_token: $scope.access_token}).then(function(success) {
+              console.log("that happened");
+//            window.localStorage['app_token'] = success.data.key;
+//            console.log(window.localStorage['app_token']);
+//            $http.defaults.headers.common['Authorization'] = "Token " + window.localStorage['app_token'];
+//            console.log("Success");
+//            $state.go('tab.play');
+//            console.log("we got code: " + $scope.app_token);
             }, function(error) {
               // console.log("couldn't get django api key");
           });
